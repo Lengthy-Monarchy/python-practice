783050c461eaba8c63a2fd008ed183cf77f944be
annei <anneirungu07@gmail.com>
2024-02-28 12:18:32 +0300

    first commit

---
+++ madlib.py
@@ -0,0 +1,7 @@
+adj = input("Adjective: ")
+Verb = input("Verb: ")
+Verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer Programming is so {adj}! It makes me so excited all the time because I love to {Verb}. Stay Hydrated and {Verb2} like you are {famous_person}"
+print(madlib)
